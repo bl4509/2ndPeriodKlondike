2afbd6e15892f09ee6b82647e8a62e36d4d068c0
matrix-lc <luke051205@gmail.com>
2022-02-22 10:37:48 -0800

    3Cards class

--- src/ThreeCards.java
+++ src/ThreeCards.java
@@ -5,7 +5,11 @@ public class ThreeCards extends Pile {
 
     @Override
     public void draw(Graphics g) {
-        // TODO Auto-generated method stub
+        int counter  = 0;
+        for(Card c : list){
+          list.draw(g, c.getX()+(i*2), c.getY()+(i*2));  
+        }
+        if(counter)
         
     }
 
